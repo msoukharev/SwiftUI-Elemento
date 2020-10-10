5ce677137c469ee1e4a78237c0ea361fe376ffc0
Max Soukharev <maxs3v@gmail.com>
2020-10-09 21:46:56 -0400

    Improve Editor

--- Sources/SwiftUIElemento/Models/Editor.swift
+++ Sources/SwiftUIElemento/Models/Editor.swift
@@ -12,6 +12,8 @@ protocol Editor: ObservableObject {
     associatedtype T
     
     var content: T { get set }
+    
+    init(content: T)
 }
 
 public class TextEditor: Editor {
@@ -24,13 +26,42 @@ public class TextEditor: Editor {
     
     @Published var isEmpty: Bool
     
-    public init(content: String) {
+    var onSubmit: Optional<() -> Void> = nil
+    
+    var onEditingChanged: Optional<() -> Void> = nil
+    
+    var onCommit: Optional<() -> Void> = nil
+    
+    required public init(content: String) {
         self.content = content
         self.isEmpty = content.isEmpty
     }
     
+    public convenience init() {
+        self.init(content: "")
+    }
+    
     public func clear() {
         self.content = ""
+    }
+    
+    public func submit() {
+        if let submit = self.onSubmit {
+            submit()
+        }
+    }
+    
+    /// TODO: encapsulate textfield's editingChanged
+//    public func editingChanged() {
+//        if let editingChanged = self.onEditingChanged {
+//            editingChanged()
+//        }
+//    }
+    
+    public func commit() {
+        if let commit = self.onCommit {
+            commit()
+        }
     }
     
 }
